1939101ce9f5f17b962c79316c72c6d430592e85
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-04-06 13:22:30 +0600

    push,4.4

--- game-off-2024/enemies/shooting_guard/states/sGuardAttack.gd
+++ game-off-2024/enemies/shooting_guard/states/sGuardAttack.gd
@@ -4,7 +4,6 @@ class_name sGuardAttack
 @export var weapon_man:Node
 
 func _process(delta: float) -> void:
-	print(weapon_man.weapon)
 	weapon_man.weapon.position = enemy_parent.position
 
 func _on_player_detector_body_exited(body: Node2D) -> void:
--- game-off-2024/guns/pickups/pick_up.gd
+++ game-off-2024/guns/pickups/pick_up.gd
@@ -7,3 +7,4 @@ var item
 
 func _ready() -> void:
 	item = item_packed.instantiate()
+	print(item)
